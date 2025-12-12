7d7dc2a12a30650b25017a41474b7ca14452fb32
Joss Vasquez <jossvaspa98@gmail.com>
2025-12-11 18:23:30 -0600

    Update Ejercicio2_Primer_Caso_Estudio.py

--- Ejercicio2_Primer_Caso_Estudio.py
+++ Ejercicio2_Primer_Caso_Estudio.py
@@ -2,18 +2,46 @@
 
 #Se invoca la librería math que permite traer las funciones tan() y radians()
 
-import math 
+#import math 
 
 #Entrada
 
-altura = 20
-angulo = int(input('Ingrese el ángulo del sol hacia el edificio: '))
+#altura = 20
+#angulo = int(input('Ingrese el ángulo del sol hacia el edificio: '))
 
 #Proceso
 #Esta formuala convierte los grados a radianes
-longitud_sombra = altura / math.tan(math.radians(angulo))
+#longitud_sombra = altura / math.tan(math.radians(angulo))
 
 #Salida
 #El formato :.2f permite mostrar solo 2 decimales en un float
-print(f'La lingitud de la sombra de un edificio de 20 metros es de: {longitud_sombra:.2f} metros')
+#print(f'La lingitud de la sombra de un edificio de 20 metros es de: {longitud_sombra:.2f} metros')
+
+
+
+
+
+import requests
+
+respuesta = requests.get("https://api.github.com")
+print("Código de respuesta:", respuesta.status_code)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
